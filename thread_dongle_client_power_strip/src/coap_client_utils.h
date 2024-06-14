7ae23e856d907a4b47f6484e4eaa0bc19160f22a
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-06-14 16:39:57 +0200

    WIP: power strip first commit

---
+++ thread_dongle_client_power_strip/src/coap_client_utils.h
@@ -0,0 +1,49 @@
+/**
+ * @file
+ * @defgroup coap_client_utils API for coap_client_* samples
+ * @{
+ */
+
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef __COAP_CLIENT_UTILS_H__
+#define __COAP_CLIENT_UTILS_H__
+
+/** @brief Type indicates function called when OpenThread connection
+ *         is established.
+ *
+ * @param[in] item pointer to work item.
+ */
+typedef void (*ot_connection_cb_t)(struct k_work *item);
+
+/** @brief Type indicates function called when OpenThread connection is ended.
+ *
+ * @param[in] item pointer to work item.
+ */
+typedef void (*ot_disconnection_cb_t)(struct k_work *item);
+
+/** @brief Initialize CoAP client utilities.
+ */
+void coap_client_utils_init(ot_connection_cb_t on_connect,
+                   ot_disconnection_cb_t on_disconnect);
+
+/** @brief Request for post keep alive msg.
+ *
+ */
+void coap_client_send_keep_alive(void);
+
+/** @brief Send a action to the CoAP server node.
+ *
+ * @note The CoAP server should be paired before to have an affect.
+ */
+void coap_client_send_command_to_server_message(uint16_t cmd_number);
+
+#endif
+
+/**
+ * @}
+ */
